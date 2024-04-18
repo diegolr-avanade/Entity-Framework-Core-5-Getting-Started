57c27ca7446fd856d98d182aaf1d52a9355c1fa2
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-18 13:29:57 +0200

    ✨Modulo 3 terminado

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -10,12 +10,14 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        private static readonly string connectionString = @"Data Source=WINAP3MGRRULDGQ\SQLEXPRESS;Initial Catalog=SamuraiAppData;Integrated Security=SSPI";
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppDataFirstLook");
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
